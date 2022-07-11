f677216c57a0443b9e34051bcedce91c1da9e7c7
brian-kipkoech-tanui <briankipkoechtanui@gmail.com>
2022-07-11 22:47:53 +0300

    static files and html

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
 from . import views
 
+app_name='pages'
 urlpatterns = [
-    path('', views.Home.as_view()),
+    path('', views.Home.as_view(), name='home'),
+    path('about', views.About.as_view(), name="about"),
+    path('services', views.Services.as_view(), name="services"),
+    path('contact', views.Contacts.as_view(), name="contacts"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,14 @@ from django.views.generic import DetailView, View, TemplateView
 
 # Create your views here.
 class Home(TemplateView):
-    template_name="home.html"
+    template_name="pages/home.html"
+    
+class About(TemplateView):
+    template_name="pages/about.html"
+    
+class Services(TemplateView):
+    template_name="pages/services.html"
+    
+class Contacts(TemplateView):
+    template_name="pages/contact.html"
+    
