3be04a7756f81be18fe0cf83ce4b71b261df45e7
brian-kipkoech-tanui <briankipkoechtanui@gmail.com>
2022-07-12 13:40:23 +0300

    Finished Teams Model

--- carzone/settings.py
+++ carzone/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'pages',
 ]
 
 MIDDLEWARE = [
@@ -78,8 +80,11 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD': '1998@Brio',
+        'HOST': 'localhost',
     }
 }
 
@@ -123,6 +128,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "carzone/static"),
 ]
+# Media Root
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from django.views.generic import DetailView, View, TemplateView
+from . models import Team
 
 # Create your views here.
 class Home(TemplateView):
     template_name="pages/home.html"
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['teams'] = Team.objects.all()
+        return context
     
 class About(TemplateView):
     template_name="pages/about.html"
+    def get_context_data(self, **kwargs):
+        # Cal the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        context["teams"] = Team.objects.all()
+        return context
+        
     
 class Services(TemplateView):
     template_name="pages/services.html"
